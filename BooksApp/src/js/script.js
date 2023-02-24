83765338fb1a7f6ddc5064404e6ab8c759767956
Przemysław Nagrodzki <nagrodzkiprzemyslaw@gmail.com>
2023-02-24 14:59:17 +0100

    Add if to initActions

--- BooksApp/src/js/script.js
+++ BooksApp/src/js/script.js
@@ -1,11 +1,13 @@
-
+{
 'use strict'
 
-render() {
+render = function
+
+render(){
     const thisBooksList = this
-    
-    for(let book in thisBooksList){
-     /* generate HTML based on template */
+
+    for(let book of thisBooksList){
+    /* generate HTML based on template */
      const generatedHTML = templates.bookTemplate({
         id: book.id,
         rice: book.price,
@@ -14,13 +16,46 @@ render() {
         rating: book.rating,
         details: book.details
      })
-     // console.log(generatedHTML)
+     console.log('generatedHTML', generatedHTML)
      /* Create element using utilis.createElementFromHTML */
      thisBooksList.element = utils.createDOMFromHTML(generatedHTML);
-     /* Find container of menu */
+     /* Find container of book */
      const bookContainer = document.querySelector(select.containerOf.book);
-     // console.log(menuContainer)
-     /* add element to menu container */ 
+      console.log('bookContainer', bookContainer)
+     /* add element to book container */ 
      bookContainer.appendChild(thisBooksList.element);
     }
+}
+
+initActions = function
+
+
+initActions(){
+const thisBooksList = this
+
+favoriteBooks = []
+
+for(let book of thisBooksList){
+    thisBooksList.bookContainer.addEventListener('dbclick', function(event){
+        event.preventDefault()
+
+        bookId = thisBooksList.getAttribute('data-id')
+    
+        if(!book.favoriteBooks.includes(bookId)){
+            thisBooksList.classList.add('favorite')
+            thisBooksList.favoriteBooks.push(bookId)
+        }
+        else {
+            thisBooksList.classList.remove('favorite')
+            const indexOfBooks = favoriteBooks.indexOf(bookId)
+            thisBooksList.favoriteBooks.splice(indexOfBooks, 1)
+        }
+    })
+}
+}
+
+render()
+
+initActions()
+
 }
